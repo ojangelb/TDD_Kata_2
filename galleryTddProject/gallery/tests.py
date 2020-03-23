aa0f52db5e514fef3198c8e8017aa322de491fb5
dvergel <dvergel@amantium.net>
2020-03-23 11:44:05 -0500

    Paso 2 : validacion de nombre de primer usuario (Rojo)

--- galleryTddProject/gallery/tests.py
+++ galleryTddProject/gallery/tests.py
@@ -24,3 +24,15 @@ class GalleryTestCase(TestCase):
         current_data = json.loads(response.content)
         print(current_data)
         self.assertEqual(len(current_data), 3)
+
+    def test_verify_first_from_images_list(self):
+        user_model = User.objects.create_user(username='test', password='kd8wke-DE34', first_name='test',
+                                              last_name='test', email='test@test.com')
+        Image.objects.create(name='nuevo', url='No', description='testImage', type='jpg', user=user_model)
+        Image.objects.create(name='nuevo2', url='No', description='testImage', type='jpg', user=user_model)
+        Image.objects.create(name='nuevo3', url='No', description='testImage', type='jpg', user=user_model)
+
+        response = self.client.get('/gallery/')
+        current_data = json.loads(response.content)
+
+        self.assertEqual(current_data[0]['fields']['name'], "nuevo2")
